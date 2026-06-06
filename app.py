c910fa3d63cf793b6a93b5753ec0466569bc5538
linetelizabethjoyal <linetelizabethjoyal@gmail.com>
2026-06-06 12:42:16 +0530

    Improve UI and update loan prediction app

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import joblib
 app = Flask(__name__)
 
 # Load pipeline
-
 pipeline = joblib.load("loan_approval_pipeline.pkl")
 
 model = pipeline["model"]
@@ -15,16 +14,76 @@ imputer = pipeline["imputer"]
 @app.route("/")
 def home():
     return render_template("index.html")
-
 @app.route("/predict", methods=["POST"])
 def predict():
-
 
     try:
 
         data = request.json
 
         df = pd.DataFrame([data])
+
+        # =========================
+        # SAFE VALUES
+        # =========================
+
+        income = max(float(df["person_income"].iloc[0]), 1)
+        emp = max(float(df["person_emp_length"].iloc[0]), 1)
+        cred = max(float(df["cb_person_cred_hist_length"].iloc[0]), 1)
+        age = max(float(df["person_age"].iloc[0]), 1)
+
+        df["loan_percent_income"] = (
+            df["loan_amnt"] / income)
+
+        # =========================
+        # FEATURE ENGINEERING
+        # =========================
+
+        df["loan_to_income_ratio"] = df["loan_amnt"] / income
+
+        df["financial_burden"] = (
+            df["loan_amnt"] * df["loan_int_rate"]
+        )
+
+        df["income_per_year_emp"] = (
+            income / emp
+        )
+
+        df["cred_hist_to_age_ratio"] = (
+            cred / age
+        )
+
+        df["int_to_loan_ratio"] = (
+            df["loan_int_rate"] / df["loan_amnt"]
+        )
+
+        df["loan_int_emp_interaction"] = (
+            df["loan_int_rate"] * emp
+        )
+
+        df["debt_to_credit_ratio"] = (
+            df["loan_amnt"] / cred
+        )
+
+        df["int_to_cred_hist"] = (
+            df["loan_int_rate"] / cred
+        )
+
+        df["int_per_year_emp"] = (
+            df["loan_int_rate"] / emp
+        )
+
+        df["loan_amt_per_emp_year"] = (
+            df["loan_amnt"] / emp
+        )
+
+        df["income_to_loan_ratio"] = (
+            income / df["loan_amnt"]
+        )
+
+        # =========================
+        # MATCH TRAINING FEATURES
+        # =========================
 
         df = df[features]
 
@@ -33,7 +92,9 @@ def predict():
             columns=features
         )
 
-        prediction = int(model.predict(df)[0])
+        prediction = int(
+            model.predict(df)[0]
+        )
 
         default_probability = float(
             model.predict_proba(df)[0][1]
@@ -51,7 +112,6 @@ def predict():
             "success": False,
             "error": str(e)
         })
-
-
+    
 if __name__ == "__main__":
     app.run(debug=True)
